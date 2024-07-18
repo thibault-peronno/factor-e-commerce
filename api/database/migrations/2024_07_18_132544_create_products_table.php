e14707e25d1e6097e92464caae42143b24016e66
thibault-peronno <peronnothibault@hotmail.fr>
2024-07-18 16:21:09 +0200

    migration works in progress

--- api/database/migrations/2024_07_18_132544_create_products_table.php
+++ api/database/migrations/2024_07_18_132544_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('description');
+            $table->integer('price', false);
+            $table->integer('available_quantity', false);
             $table->timestamps();
         });
     }
