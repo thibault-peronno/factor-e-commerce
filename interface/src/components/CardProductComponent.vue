71dd8ed6626919affefe8303b3b620c140db9570
thibault-peronno <peronnothibault@hotmail.fr>
2024-07-22 10:35:52 +0200

    card work in progress

--- interface/src/components/CardProductComponent.vue
+++ interface/src/components/CardProductComponent.vue
@@ -1,29 +1,66 @@
 <script setup lang="ts">
 
+defineProps(['productValue'])
 </script>
 
 <template>
     <div>
-        <img src="" alt="image du produit">
-        <h2>Nom du produit</h2>
-        <p>nombre de produit</p>
-        <p>description du produit</p>
-        <p>categorie du produit</p>
+        <img :src="productValue.picture" alt="image du produit">
+        <span class="card_cat-stock">
+            <p class="cat">{{ productValue.category }}</p>
+            <p class="stock">Stock : {{ productValue.available_quantity}}</p>
+        </span>
+        <h2>{{ productValue.name }}</h2>
+        <p>{{ productValue.description }}</p>
         <button class="montserrat-alternates-medium">Ajouter</button>
     </div>
 </template>
 
 <style scoped>
-div{
-    padding: 2rem;
+div {
+    padding: 1rem;
     background-color: #FFFFFF;
     border-radius: 2px;
-    width: 300px;
-    height: 280px;
+    max-width: 350px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 }
 
-button{
+h2{
+    color: #048A84;
+    margin: 0.5rem 0;
+}
+
+img{
+    width: 100%;
+    margin: 0 0 0.5rem 0;
+}
+
+.cat{
+    color: #908080;
+    font-size: small;
+    background-color: #DDF7F5;
+    border-radius: 100px;
+    width: 50px;
+    text-align: center;
+}
+
+.stock{
+    color: #908080;
+    font-size: small;
+}
+
+.card_cat-stock{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+button {
     background-color: #64A5C4;
     width: 100%;
+    margin: 1.5rem 0 0 0;
+    font-weight: bold;
 }
 </style>
--- interface/src/components/homeComponent.vue
+++ interface/src/components/homeComponent.vue
@@ -12,7 +12,31 @@ onMounted(() => {
 
 <template>
   <h1>Découvre nos produits</h1>
-  <div v-for="product in productsStore.products"  :key="product.id">
-    <CardProductComponent />
+  <div class="divCard">
+    <CardProductComponent v-for="product in productsStore.products" :key="product.id" :productValue=product />
   </div>
 </template>
+
+
+<style scoped>
+.divCard {
+  display: flex;
+  gap: 2.5rem;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+@media (min-width: 760px) {
+  .divCard {
+  justify-content: space-evenly;
+  gap: 1rem;
+}
+}
+
+@media (min-width: 1024px) {
+  .divCard {
+  justify-content: space-evenly;
+  gap: 2.5rem;
+}
+}
+</style>
