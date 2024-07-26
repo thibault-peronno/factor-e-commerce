090feeb7cb901745637e627502dcd6cfa6cbaf60
thibault-peronno <peronnothibault@hotmail.fr>
2024-07-26 18:25:58 +0200

    user view do not dynamic

---
+++ interface/src/components/UserComponent.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import RetireProductComponent from './RetireProductComponent.vue'
+</script>
+
+<template>
+  <h1 class="account">Votre compte</h1>
+  <div class="account_picture-datas">
+    <i class="pi pi-user" style="color: #048a84; font-size: 5rem"></i>
+    <div class="account_datas montserrat-alternates-bold">
+      <p>Thibault Peronno</p>
+    </div>
+  </div>
+  <RetireProductComponent />
+</template>
+
+<style scoped>
+.account_picture-datas {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 4rem;
+}
+
+.account_datas {
+  color: #048a84;
+  font-size: 2rem;
+}
+
+@media (min-width: 760px) {
+  .account_picture-datas {
+    justify-content: start;
+  }
+}
+</style>
