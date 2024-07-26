6eeeda857bd381a70c4668d538e487c64e911fa2
thibault-peronno <peronnothibault@hotmail.fr>
2024-07-26 15:44:37 +0200

    prettier

--- interface/src/stores/cart.ts
+++ interface/src/stores/cart.ts
@@ -1,129 +1,128 @@
-import { ref, computed } from 'vue';
-import { defineStore } from 'pinia';
-import axios, { type AxiosResponse } from 'axios';
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
 
 interface productCart {
-  id: number,
-  name: string,
-  description: string,
-  picture: string,
-  price: number,
-  cart_id: number,
+  id: number
+  name: string
+  description: string
+  picture: string
+  price: number
+  cart_id: number
 }
 
 interface AddProductResponse {
-  message: string,
-	cart_id: number
+  message: string
+  cart_id: number
 }
 
-interface productReponse {
-data: AddProductResponse,
-status: number
-statusText: string
+interface productResponse {
+  data: AddProductResponse
+  status: number
+  statusText: string
 }
 
-type productsInCart = productCart[];
+type productsInCart = productCart[]
 
 export const useCartStore = defineStore('cart', () => {
-  const cartIsactive = ref(false);
-  const cartProducts = ref<productsInCart>([]);
-  const countProductsInCart = ref(0);
+  const cartIsactive = ref(false)
+  const cartProducts = ref<productsInCart>([])
+  const countProductsInCart = ref(0)
 
   const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
-    withCredentials: true,
+    baseURL: 'http://127.0.0.1:8000/api/',
+    withCredentials: true
   })
 
   async function addProductInCart(newProductInCArt: productCart) {
     try {
-        const responseAxios = await addProductInCartDB(newProductInCArt);
-        console.info(responseAxios);
-        
-        if (responseAxios.statusText === 'Created') {
-          newProductInCArt.cart_id = responseAxios.data.cart_id;
-          console.log(newProductInCArt, responseAxios.data.cart_id);
-            cartProducts.value.push(newProductInCArt);
-            updateCountProductInCArt();
-        }
+      const responseAxios = await addProductInCartDB(newProductInCArt)
+
+      if (responseAxios.statusText === 'Created') {
+        newProductInCArt.cart_id = responseAxios.data.cart_id
+
+        cartProducts.value.push(newProductInCArt)
+        updateCountProductInCArt()
+      }
     } catch (error) {
-        console.error("Erreur lors de l'ajout du produit au panier:", error);
+      console.error("Erreur lors de l'ajout du produit au panier:", error)
     }
-}
-
-
-
-/*
-* Pour user_id, on devrait récuperer l'information 
-* depuis un store user, initialisé au moment de la connexion.
-*/
-async function addProductInCartDB(newProduct: productCart): Promise<productReponse> {
-  const payloadProductCart = {
-    quantity: 1,
-    product_id: newProduct.id,
-    user_id: 1,
-  };
-  
-  // Stockez la promesse dans une variable temporaire
-  const promise = axiosInstance.post('cart', payloadProductCart)
-  .then((response) => {
-    return response; // Assurez-vous de retourner la promesse résolue ici
-  })
-  .catch((error) => {
-    console.error(error);
-    throw error; // Propagez l'erreur pour qu'elle soit gérée par le code appelant
-  });
-  
-  // Retournez la promesse stockée
-  return promise;
-}
-
-async function removeProductInCart(cart_id : number) {
-  try {
-    const responseAxios = await updateIsRetireInCartDB(cart_id);
-    console.info(responseAxios.status);
-
-    if(responseAxios.status == 204){
-      const cart = cartProducts.value.filter((cartProduct) =>cartProduct.cart_id !== cart_id)
-
-      cartProducts.value = cart;
-      updateCountProductInCArt();
-    }
-  } catch (error) {
-    console.error("Erreur lors de la suppressiondu produit du panier:", error);
-  }
-}
-
-async function updateIsRetireInCartDB(cart_id : number){
-  const payloadRetire = {
-    id:cart_id,
-    is_retire: 1
   }
 
-  const promise = axiosInstance.put('cart/' + cart_id, payloadRetire)
-  .then((response) => {
-    return response; // Assurez-vous de retourner la promesse résolue ici
-  })
-  .catch((error) => {
-    console.error(error);
-    throw error; // Propagez l'erreur pour qu'elle soit gérée par le code appelant
-  });
-  
-  // Retournez la promesse stockée
-  return promise;
-}
+  /*
+   * Pour user_id, on devrait récuperer l'information
+   * depuis un store user, initialisé au moment de la connexion.
+   */
+  async function addProductInCartDB(newProduct: productCart): Promise<productResponse> {
+    const payloadProductCart = {
+      quantity: 1,
+      product_id: newProduct.id,
+      user_id: 1
+    }
 
+    // Stockez la promesse dans une variable temporaire
+    const promise = axiosInstance
+      .post('cart', payloadProductCart)
+      .then((response) => {
+        return response // Assurez-vous de retourner la promesse résolue ici
+      })
+      .catch((error) => {
+        console.error(error)
+        throw error // Propagez l'erreur pour qu'elle soit gérée par le code appelant
+      })
 
+    // Retournez la promesse stockée
+    return promise
+  }
 
-function updateCountProductInCArt() {
-countProductsInCart.value = cartProducts.value.length;
-}
+  async function removeProductInCart(cart_id: number) {
+    try {
+      const responseAxios = await updateIsRetireInCartDB(cart_id)
+
+      if (responseAxios.status == 204) {
+        const cart = cartProducts.value.filter((cartProduct) => cartProduct.cart_id !== cart_id)
+
+        cartProducts.value = cart
+        updateCountProductInCArt()
+      }
+    } catch (error) {
+      console.error('Erreur lors de la suppressiondu produit du panier:', error)
+    }
+  }
+
+  async function updateIsRetireInCartDB(cart_id: number) {
+    const payloadRetire = {
+      id: cart_id,
+      is_retire: 1
+    }
+
+    const promise = axiosInstance
+      .put('cart/' + cart_id, payloadRetire)
+      .then((response) => {
+        return response
+      })
+      .catch((error) => {
+        console.error(error)
+        throw error
+      })
+
+    return promise
+  }
+
+  function updateCountProductInCArt() {
+    countProductsInCart.value = cartProducts.value.length
+  }
 
   function toggleCart() {
-    cartIsactive.value = !cartIsactive.value;
+    cartIsactive.value = !cartIsactive.value
   }
 
-
-  
-
-  return { cartIsactive,  cartProducts, countProductsInCart, toggleCart, addProductInCart, removeProductInCart }
+  return {
+    cartIsactive,
+    cartProducts,
+    countProductsInCart,
+    toggleCart,
+    addProductInCart,
+    removeProductInCart
+  }
 })
--- interface/src/stores/detailsProduct.ts
+++ interface/src/stores/detailsProduct.ts
@@ -1,17 +1,17 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
-import axios from 'axios';
+import axios from 'axios'
 import { useRoute } from 'vue-router'
 
 interface product {
-  id: number,
-  name: string,
-  description: string,
-  picture: string,
-  price: number,
-  category: string,
-  available_quantity: number,
-  created_at: string,
+  id: number
+  name: string
+  description: string
+  picture: string
+  price: number
+  category: string
+  available_quantity: number
+  created_at: string
   updated_at: string
 }
 
@@ -20,7 +20,7 @@ export const useDetailsProductStore = defineStore('detailsProduct', () => {
   const product = ref<product>({
     id: 0,
     name: 'Non trouvé',
-    description: 'Le produit n\'a pas été trouvé',
+    description: "Le produit n'a pas été trouvé",
     picture: '',
     price: 0,
     category: 'introuvable',
@@ -29,23 +29,25 @@ export const useDetailsProductStore = defineStore('detailsProduct', () => {
     updated_at: ''
   })
 
-  const route = useRoute();
+  const route = useRoute()
 
   const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
-    withCredentials: true,
+    baseURL: 'http://127.0.0.1:8000/api/',
+    withCredentials: true
   })
-  
-    async function callProductApi () {
-      await axiosInstance.get<product>("product/"+route.params.productId)
-    .then((response) => {
-      product.value = response.data
-      return
-    }).catch((error) =>{
-      console.log(error);
-      return error;
-    })
-    } 
+
+  async function callProductApi() {
+    await axiosInstance
+      .get<product>('product/' + route.params.productId)
+      .then((response) => {
+        product.value = response.data
+        return
+      })
+      .catch((error) => {
+        console.log(error)
+        return error
+      })
+  }
 
   return { value, product, callProductApi }
 })
