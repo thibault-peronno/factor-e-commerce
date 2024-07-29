9e842894a8b5b92ed05c12e608680f704f288e7a
thibault-peronno <peronnothibault@hotmail.fr>
2024-07-29 17:28:29 +0200

    fix

--- interface/src/components/DetailsProductComponent.vue
+++ interface/src/components/DetailsProductComponent.vue
@@ -33,7 +33,7 @@ onMounted(() => {
                 showButtons
                 buttonLayout="vertical"
                 style="width: 3rem"
-                :min="0"
+                :min="1"
                 :max="detailsProduct.product.available_quantity"
               >
                 <template #incrementbuttonicon>
